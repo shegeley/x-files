cc260a9e7fc3cd703e36fc0b9af046a236dd6437
Grigory Shepelev <shegeley@gmail.com>
2023-04-19 08:22:53 +0300

    utils/project: add git-project-dir

---
+++ x-files/utils/project.scm
@@ -0,0 +1,22 @@
+(define-module (x-files utils project)
+  #:use-module (ice-9 ftw)
+  #:use-module (ice-9 match)
+
+  #:export (git-project-dir))
+
+(define* (git-project-dir
+          #:optional (start-dir (dirname (or (current-filename) "."))))
+  (let* ((up (lambda (x) (string-append x "/../")))
+         (start-dir (canonicalize-path start-dir))
+         (split (string-split start-dir #\/)))
+    (match split
+      (("" "home" x) #f)
+      (("" "home" x "") #f)
+      (("" "home" x ...)
+       (let* ((scan
+               (scandir start-dir
+                        (lambda (x) (equal? x ".git")))))
+         (match scan
+           ('(".git") start-dir)
+           ('() (git-project-dir
+                 (up start-dir)))))))))
