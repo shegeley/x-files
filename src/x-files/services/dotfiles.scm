b27a1b9150dd44298262097df986ea5d787f0f4b
Grigory Shepelev <shegeley@gmail.com>
2023-06-12 13:55:29 +0300

    services/dotfiles: use `home-files-service-type`

--- src/x-files/services/dotfiles.scm
+++ src/x-files/services/dotfiles.scm
@@ -1,4 +1,6 @@
 (define-module (x-files services dotfiles)
+  #:use-module (srfi srfi-1)
+
   #:use-module (gnu home)
   #:use-module (gnu services)
   #:use-module (gnu packages)
@@ -61,71 +63,34 @@
   (map
    (match-lambda
      ((home-dir-loc config-dir-loc)
-      (let ((home-dir-loc (-home- home-dir-loc))
-            (config-dir-loc (storage config-dir-loc))
-            (f (lambda (x) (local-file x))))
-        #~(begin
-            (format #t "Symlinking ~a with ~a ~%" #$home-dir-loc #$config-dir-loc)
-            (cond ((and (file-exists? #$home-dir-loc)
-                        (not (file-exists? #$config-dir-loc)))
-                   (begin
-                     (rename-file #$home-dir-loc #$config-dir-loc)
-                     (symlink #$config-dir-loc (f #$home-dir-loc))
-                     (format #t "Dir ~s was not found. Moving ~s to config dir and symlinking. ~%" #$config-dir-loc #$home-dir-loc)))
-                  ((and (file-exists? #$home-dir-loc)
-                        (file-exists? #$config-dir-loc))
-                   (begin
-                     (delete-file-recursively #$home-dir-loc)
-                     (symlink #$config-dir-loc (f #$home-dir-loc))
-                     (format #t "File ~s was found. Deleting it and symlinking it to ~s. ~%"  #$home-dir-loc #$config-dir-loc)))
-                  ((and (not (file-exists? #$home-dir-loc))
-                        (file-exists? #$config-dir-loc))
-                   (begin
-                     (symlink #$config-dir-loc (f #$home-dir-loc))
-                     (format #t "File ~s was not found. Symlinking it with ~s. ~%"  #$home-dir-loc #$config-dir-loc)))
-                  ((and (not (file-exists? #$home-dir-loc))
-                        (not (file-exists? #$config-dir-loc)))
-                   (format #t "File/Directory ~s doesn't exists, neither file/directory ~s exists. Can't synchronize these dotfiles. ~%" #$home-dir-loc #$config-dir-loc))
-                  (else
-                   (format #t "Don't know how to handle this case syncronizing ~s and ~s. ~%" #$home-dir-loc #$config-dir-loc))))))) scheme))
+      (let* ((home-dir-loc (-home- home-dir-loc))
+             (config-dir-loc (storage config-dir-loc))
+             (f (lambda (x) (local-file
+                        x #:recursive?
+                        (if (directory-exists? config-dir-loc)
+                            #t #f)))))
+        (list home-dir-loc (f config-dir-loc))))) scheme))
 
-(define (activation config)
-  (match-record config <dotfile-manager-conf>
-    (storage schemes)
-    (let ((schemes (normalize-schemes schemes)))
-      (with-imported-modules
-          (source-module-closure
-           '((x-files utils files)
-             (ice-9 format)
-             (ice-9 match))
-           #:select?
-           (lambda (name)
-             (or
-              ((@@ (guix modules) guix-module-name?) name)
-              (match name
-                (('x-files 'utils ..) #t)
-                (else #f)))))
-        #~(begin
-            (use-modules
-             (ice-9 match)
-             (ice-9 format)
-             (x-files utils files))
-
-            #$@(map
-                (match-lambda
-                  ((package scheme)
-                   #~(if (member #$package
-                                 (quasiquote #$%keys-packages))
-                         (format #t "Don't know how to synchronize keys yet. TODO: Think about it. ~%")
-                         #$@(sync-package-files storage scheme))))
-                schemes)
-
-            (format #t "Done syncronizing dotfiles. ~%"))))))
+(define (files config)
+  (match-record
+   config <dotfile-manager-conf>
+   (storage schemes)
+   (let* ((schemes (normalize-schemes schemes))
+          (acc '()))
+     (map
+      (match-lambda
+        ((package scheme)
+         (if (member package %keys-packages)
+             (format #t "Don't know how to synchronize keys yet. TODO: Think about it. ~%")
+             (set! acc (append acc (sync-package-files storage scheme))))))
+      schemes)
+     acc)))
 
 (define-public dotfile-manager-service-type
   (service-type
    (name 'dotfile-manager)
    (extensions
-    (list (service-extension home-activation-service-type activation)))
+    (list
+     (service-extension home-files-service-type files)))
    (description
     "Simple service to keep up all your dotfiles. In general this is very dirty and quick solution. It'd be better to manage them as gexp-generated. TODO.")))
