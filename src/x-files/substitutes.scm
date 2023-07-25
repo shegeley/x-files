10fc719b061f9218207d98b6995d272e527da3f6
Grigory Shepelev <shegeley@gmail.com>
2023-07-25 10:27:11 +0300

    x-files/substitutes: add public substitutes

---
+++ src/x-files/substitutes.scm
@@ -0,0 +1,13 @@
+(define-module (x-files substitutes)
+
+  #:export (substitutes:public))
+
+(define substitutes:public
+  (list
+   "https://mirror.guix.org.cn"
+   "https://mirror.sjtu.edu.cn/guix" ;; from https://guix.org.cn/wiki/mirror
+   "https://substitutes.nonguix.org"
+   "http://ci.guix.trop.in"
+   "https://bordeaux.guix.gnu.org"
+   "https://guix.tobias.gr/substitutes"
+   "https://substitutes.guix.psychnotebook.org"))
