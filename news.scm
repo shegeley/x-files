766244a1e124a3311e8c629508db95116f8ccc79
Grigory Shepelev <shegeley@gmail.com>
2023-11-12 17:43:44 +0300

    + news

---
+++ news.scm
@@ -0,0 +1,6 @@
+(channel-news
+ (version 0)
+ (entry
+  (commit "ea6f74f6e287774d0723748ab8a610ee4fe7a2c7")
+  (title (en "Storage: first implementation"))
+  (body (en "First attempt at unifying URI RFC 3986-based resource access"))))
