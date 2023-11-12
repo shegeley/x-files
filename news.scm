47a070f0fc2352a7cc43c77e199c4c30fe8e3266
Grigory Shepelev <shegeley@gmail.com>
2023-11-12 17:46:05 +0300

    news: news on news

--- news.scm
+++ news.scm
@@ -3,4 +3,8 @@
  (entry
   (commit "ea6f74f6e287774d0723748ab8a610ee4fe7a2c7")
   (title (en "Storage: first implementation"))
-  (body (en "First attempt at unifying URI RFC 3986-based resource access"))))
+  (body (en "First attempt at unifying URI RFC 3986-based resource access")))
+ (entry
+  (commit "cab1244d30b8ec268e3bef6ef8d4d3a689b20318")
+  (title (en "Breaking @code{news}!"))
+  (body (en "Add news to this channel. TODO: automatize the management"))))
