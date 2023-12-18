8e5277be9eb220bed8d8186cb0e537a1f0fb84e6
Grigory Shepelev <shegeley@gmail.com>
2023-12-18 08:34:56 +0300

    + news on docker

--- news.scm
+++ news.scm
@@ -1,6 +1,10 @@
 (channel-news
  (version 0)
  (entry
+  (commit "da6ac8a945556f6b6210273fb7a173d5d5869144")
+  (title (en "Docker: re-implementation + fresh packages"))
+  (body (en "Tweaked the original @code{(@@ (gnu services docker) docker-service-type)} to support @code{/etc/docker/daemon.json} settings file")))
+ (entry
   (commit "ea6f74f6e287774d0723748ab8a610ee4fe7a2c7")
   (title (en "Storage: first implementation"))
   (body (en "First attempt at unifying URI RFC 3986-based resource access")))
