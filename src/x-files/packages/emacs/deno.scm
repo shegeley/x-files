2e941ccb3ebd280767d4b0410eb0cce0ecc68bd8
Grigory Shepelev <shegeley@gmail.com>
2025-02-20 22:22:40 +0300

    (x-files packages emacs deno): + emacs-deno-mode

--- src/x-files/packages/emacs/deno.scm
+++ src/x-files/packages/emacs/deno.scm
@@ -1,7 +1,8 @@
 (define-module (x-files packages emacs deno)
-    #:use-module ((guix licenses) #:prefix license:)
+  #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (guix download)
+  #:use-module (guix git-download)
   #:use-module (guix gexp)
   #:use-module (guix build-system emacs)
   #:use-module (gnu packages emacs-xyz)
@@ -25,4 +26,22 @@
     (description "`deno-ts-mode' is derived from `typescript-ts-mode' so it depends on the same TypeScript tree-sitter parsers.  Install both the TSX and TypeScript parsers for full deno syntax support (see README for full details).
 
 This package helps solve some of the problems that arise from Deno and TypeScript sharing the same file extension.  If a Deno configuration file is found at project root, `deno-ts-mode' takes precedence over `typescript-ts-mode'.  Both .ts and .tsx file extensions are supported.")
+    (license license:gpl3+)))
+
+(define-public emacs-deno-mode
+  (package
+    (name "emacs-deno-mode")
+    (version "v0.1.0")
+    (source
+     (origin
+      (method git-fetch)
+      (uri (git-reference
+            (url "https://github.com/shegeley/deno-mode")
+            (commit version)))
+      (sha256 (base32 "0xyzhiy4mhmwhvp7w9lxbpjq7sqjrp7f32yv4a8jl2l20927xbgq"))))
+    (build-system emacs-build-system)
+    (propagated-inputs (list emacs-eglot emacs-project))
+    (home-page "https://github.com/shegeley/deno-mode")
+    (synopsis "Major mode for Deno")
+    (description "`deno-mode' is derived from `typescript-ts-mode' & `js-ts-mode' so it depends on the same TypeScript tree-sitter parsers.")
     (license license:gpl3+)))
