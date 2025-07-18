227c0ef69bbd9ffb4792bb952062e74b3dd59b58
Grigory Shepelev <shegeley@gmail.com>
2025-07-18 14:23:08 +0300

    (x-files packages minio): + minio-client

--- src/x-files/packages/minio.scm
+++ src/x-files/packages/minio.scm
@@ -33,3 +33,31 @@
    (synopsis "High-performance, S3 compatible object store")
    (description "Designed for speed and scalability, MinIO powers AI/ML, analytics, and data-intensive workloads with industry-leading performance")
    (license license:agpl3+))))
+
+(define-public minio-client
+ (let* [(tag "RELEASE.2025-05-21T01-59-54Z")
+        (binary-filename (string-append "mc." tag))
+        (hash "092dx74xalmk5x3585l3xs5yjcvrg8k6q4p1iligp0fpm51ca4gv")]
+  (package
+   (name "minio-client")
+   (version tag)
+   (source (origin
+            (method url-fetch)
+            (uri (string-append
+                  "https://dl.min.io/client/mc/release/linux-amd64/"
+                  binary-filename))
+            (sha256 (base32 hash))))
+   (build-system copy-build-system)
+   (arguments
+    (list
+     #:install-plan #~`(("mc" "/bin/mc"))
+     #:phases #~(modify-phases %standard-phases
+                 (replace 'unpack
+                  (lambda* (#:key source #:allow-other-keys)
+                   (copy-file source "mc")
+                   (chmod "mc" #o755))))))
+   (supported-systems '("x86_64-linux"))
+   (home-page "https://min.io")
+   (synopsis "MinIO Client")
+   (description "MinIO Client")
+   (license license:agpl3+))))
