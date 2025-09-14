1ad370ed9f5b7f3fd49ff9c260b2007e606e3ffe
Grigory Shepelev <shegeley@gmail.com>
2025-09-14 17:41:05 +0300

    (x-files services dconf): refactoring & dump resulted .ini to ./config/dconf/profile.ini

--- src/x-files/services/dconf.scm
+++ src/x-files/services/dconf.scm
@@ -1,13 +1,4 @@
 (define-module (x-files services dconf)
-
-  ;; #:use-module (ini)
-
-  #:use-module (ice-9 popen)
-  #:use-module (ice-9 rdelim)
-  #:use-module (ice-9 format)
-  #:use-module (ice-9 textual-ports)
-  #:use-module (ice-9 match)
-
   #:use-module (x-files utils records)
 
   #:use-module (gnu services)
@@ -23,9 +14,7 @@
             dconf-service-conf))
 
 (define guile-ini:extensions
-  (list guile-ini
-        guile-smc
-        guile-lib))
+  (list guile-ini guile-smc guile-lib))
 
 ;; (define dump
 ;;   (with-extensions guile-ini:extensions
@@ -49,35 +38,25 @@
 ;;      ,(string-append (getenv "XDG_CONFIG_HOME")
 ;;                      "./dconf/profiles/main"))))
 
-;; (define dconf-profile
-;;   (string-append
-;;    (getenv "XDG_CONFIG_HOME")
-;;    "/dconf/"
-;;    "main.ini"))
+(define (serialize-ini-gexp entries)
+  (with-extensions guile-ini:extensions
+    #~(begin
+        (use-modules (ini))
+        (call-with-output-file #$output
+          (lambda (port) (scm->ini #$entries #:port port))))))
 
-;; (define (files config)
-;;   (let ((entries (dconf-service-conf:entries config)))
-;;     (list
-;;      (list dconf-profile
-;;            (computed-file
-;;             (basename dconf-profile)
-;;             (with-extensions guile-ini:extensions
-;;               #~(begin
-;;                   (use-modules (ini))
-;;                   (call-with-output-file #$output
-;;                     (lambda (port)
-;;                       (scm->ini
-;;                        (quote #$entries)
-;;                        #:port port))))))))))
+(define (files config)
+  (let ((entries (dconf-service-conf:entries config)))
+    `(("dconf/profile.ini"
+       ,(computed-file
+         (basename "profile.ini")
+         (serialize-ini-gexp entries))))))
 
 (define (activation config)
   (let ((entries (dconf-service-conf:entries config)))
     (with-extensions guile-ini:extensions
       #~(begin
-          (use-modules
-           (ini)
-           (ice-9 popen)
-           (ice-9 textual-ports))
+          (use-modules (ini) (ice-9 popen) (ice-9 textual-ports))
 
           (let [(p (open-output-pipe
                     (string-append
@@ -91,9 +70,8 @@
    (name 'dconf-settings)
    (extensions
     (list
-     (service-extension
-      home-activation-service-type
-      activation)))
+     (service-extension home-xdg-configuration-files-service-type files)
+     (service-extension home-activation-service-type activation)))
    (default-value (dconf-service-conf))
    (description
     "A simple service to set all the proper dconf keys to manage Gnome desktop (Custom keyboard shortcuts, applications colorschemes and etc).
@@ -120,3 +98,62 @@
      @end example
      NOTE: dconf-service-type already exists in (gnu services xorg). But it system (not home) service and too complicated to sucha little job it's should do.
     ")))
+
+;; (define entries
+;;   (let [(emacsclient (file-append emacs-pgtk "/bin/emacsclient"))]
+;;     #~`(("org/gnome/GPaste"
+;;          ("history-name" . "'history'")
+;;          ("track-changes" . "true")
+;;          ("track-extension-state" . "false"))
+;;         ("org/gnome/mutter/keybindings"
+;;          ;; gsettings set org.gnome.mutter.keybindings switch-monitor "[]"
+;;          ;; Fucking Super+p shortcut that mess up my external monitors setup
+;;          ;; I misclick it way too often
+;;          ("switch-monitor" . "[]"))
+;;         ("org/gnome/desktop/interface"
+;;          ("color-scheme" . "'prefer-dark'")
+;;          ("gtk-theme" . "'Materia-dark'")
+;;          ("icon-theme" . "'Flat-Remix-Black-Dark'")
+;;          ("show-battery-percentage" . "true"))
+;;         ("org/gnome/desktop/input-sources"
+;;          ("mru-sources" . "[('xkb', 'us'), ('xkb', 'ru')]")
+;;          ("per-window" . "false")
+;;          ("sources" . "[('xkb', 'us'), ('xkb', 'ru')]")
+;;          ;; lv3:switch = alternate characters key (right ctrl), compose = right alt
+;;          ("xkb-options" . "['grp:win_space_toggle' , 'lv3:switch', 'compose:ralt']"))
+;;         ("org/gnome/desktop/sceensaver"
+;;          ("color-shading-type" . "'solid'")
+;;          ("lock-delay" . "uint32 0")
+;;          ("lock-enabled" . "false")
+;;          ("picture-options" . "'fill'")
+;;          ("primary-color" . "'#3465a4'")
+;;          ("secondary-color" . "'#000000'"))
+;;         ("org/gnome/desktop/background"
+;;          ("color-shading-type" . "'solid'")
+;;          ("lock-delay" . "uint32 0")
+;;          ("lock-enabled" . "false")
+;;          ("picture-options" . "'fill'")
+;;          ("primary-color" . "'#3465a4'")
+;;          ("secondary-color" . "'#000000'"))
+;;         ("org/gnome/GPaste"
+;;          ("history-name" . "'history'")
+;;          ("track-extension-state" . "false"))
+;;         ("org/gnome/shell"
+;;          ("disable-user-extensions" . "false")
+;;          ("disabled-extensions" . "['clipboard-indicator@tudmotu.com']")
+;;          ("enabled-extensions" . "['user-theme@gnome-shell-extensions.gcampax.github.com', 'GPaste@gnome-shell-extensions.gnome.org']")
+;;          ("remember-mount-password" . "true"))
+;;         ("org/gnome/settings-daemon/plugins/media-keys"
+;;          ("custom-keybindings" . "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"))
+;;         ("org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0"
+;;          ("binding" . "'<Control><Alt>t'")
+;;          ("command" . ,(string-append "'" #$emacsclient " " "-c" " " "-e" " " "\"(vterm)\"" "'"))
+;;          ("name" . "'Emacs Vterm'"))
+;;         ("org/gnome/GPaste"
+;;          ("history-name" . "'history'")
+;;          ("track-extension-state" . "false")))))
+
+;; ((@ (rde api store) build-with-store)
+;;  (computed-file
+;;   (basename dconf-profile)
+;;   serialize-ini-gexp))
