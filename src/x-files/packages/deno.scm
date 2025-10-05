c18d8478fcd66c2cd8f4076034c10c0c93bb70ea
Grigory Shepelev <shegeley@gmail.com>
2025-10-05 11:04:15 +0300

    (x-files packages deno): 2.2.12 -> 2.5.3

--- src/x-files/packages/deno.scm
+++ src/x-files/packages/deno.scm
@@ -9,28 +9,32 @@
   #:use-module (ice-9 match)
   #:use-module ((guix licenses) #:prefix license:))
 
+(define target->bin-name
+  '(("x86_64-linux"  . "deno-x86_64-unknown-linux-gnu.zip")
+    ("aarch64-linux" . "deno-aarch64-unkown-linux-gnu.zip")))
+
+(define targets (map car target->bin-name))
+
+(define target->hash
+  '(("x86_64-linux"  . "0rza5bgzv6mbm57qikgq5fml680j0h4363pfdl5by5lnphgxs6j2")
+    ("aarch64-linux" . "1wdn33vc4lxz6kva0wdhj7qqdqmvfwira3hlx7rlmydfdfx2rbbq")))
+
 (define-public deno
-  (let* [(target (or (%current-target-system) (%current-system)))
-         (deno.bin-name
-          (string-append
-           "deno-"
-           (match target
-             ("x86_64-linux"  "x86_64")
-             ("aarch64-linux" "aarch64"))
-           "-unknown-linux-gnu.zip"))
-         (hash
-          (match target
-            ("x86_64-linux"  "1mb6vn1srwqlp4dlyky6lp433kfgb4srrrq76ngjzq6h6fxghzmv")
-            ("aarch64-linux" "0x1062r0m5xg950l0zgpibkmz16fbppgv7s6ixgc2dkjjikg6m8p")))]
+  (let* [(target    (or (%current-target-system) (%current-system)))
+         (deno.bin  (assoc-ref target->bin-name target))
+         (hash      (assoc-ref target->hash target))
+         (version   "2.5.3")
+         (uri       (string-append
+                     "https://github.com/denoland/"
+                     "deno/releases/download/"
+                     "v" version
+                     "/" deno.bin))]
     (package
       (name "deno")
-      (version "2.2.12")
+      (version version)
       (source (origin
                 (method url-fetch/zipbomb)
-                (uri (string-append "https://github.com/denoland/"
-                                    "deno/releases/download/"
-                                    "v" version
-                                    "/" deno.bin-name))
+                (uri uri)
                 (sha256 (base32 hash))))
       (build-system binary-build-system)
       (arguments
@@ -43,7 +47,7 @@
             (add-after 'unpack 'chmod
               (lambda _ (chmod "./deno" #o755))))))
       (inputs (list `(,gcc "lib")))
-      (supported-systems '("x86_64-linux" "aarch64-linux"))
+      (supported-systems targets)
       (home-page "https://deno.com/")
       (synopsis "A modern runtime for JavaScript and TypeScript")
       (description "Deno (/ˈdiːnoʊ/, pronounced dee-no) is a JavaScript, TypeScript, and WebAssembly runtime with secure defaults and a great developer experience. It's built on V8, Rust, and Tokio.")
