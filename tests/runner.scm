3be544c584a01c0afc633194726b68744b8ce9f9
Grigory Shepelev <mail@grigory.tech>
2026-04-02 10:33:09 +0300

    tests: (srfi srfi-64-ext) -> (ares suitbl)

--- tests/runner.scm
+++ tests/runner.scm
@@ -1,17 +1,38 @@
 (use-modules
- (srfi srfi-64-ext)
+ (ares suitbl)
  (guix modules)
  (guix build utils))
 
 (define files
   (let ((prefix "./tests/"))
-    (map (lambda (x)  (string-drop x (string-length prefix)))
+    (map (lambda (x) (string-drop x (string-length prefix)))
          (find-files (string-append prefix "x-files")))))
 
 (define modules
   (map (compose resolve-module file-name->module-name) files))
 
+(define (module-public-test-suites module)
+  (filter identity
+          (module-map
+           (lambda (k v)
+             (and (variable-bound? v)
+                  (let ((val (variable-ref v)))
+                    (and (procedure? val)
+                         (procedure-property val 'suitbl-test-suite?)
+                         val))))
+           (module-public-interface module))))
+
 (define (run!)
-  (run-project-tests-cli modules))
+  (let* ((runner (make-suitbl-test-runner))
+         (suites (apply append (map module-public-test-suites modules)))
+         (root  (test-suite-thunk "x-files"
+                  (for-each (lambda (s) (s)) suites))))
+    (run-test-suites runner (list root))
+    (let ((summary (runner '((type . get-run-summary)))))
+      (format #t "\n~a\n" summary)
+      (when (> (+ (assoc-ref summary 'failures)
+                  (assoc-ref summary 'errors))
+               0)
+        (exit 1)))))
 
 (run!)
--- tests/x-files/tests/storage.scm
+++ tests/x-files/tests/storage.scm
@@ -1,22 +1,19 @@
 (define-module (x-files tests storage)
   #:use-module (x-files storage)
   #:use-module (x-files utils tests)
+  #:use-module (ares suitbl)
+  #:use-module (srfi srfi-1))
 
-  #:use-module (srfi srfi-1)
-  #:use-module (srfi srfi-64)
-  #:use-module (srfi srfi-64-ext test))
-
-(define-test git-subdir-storage
-  (test-group "git-subdir-storage"
+(define-test-suite git-subdir-storage-tests
+  (test "add and get a file"
     (with-test-dir
      "storage"
      (lambda (d)
-       (let* [(test-file (string-append (%test-dir) "/file.txt"))
+       (let* ((test-file (string-append (%test-dir) "/file.txt"))
               (_ (with-output-to-file test-file
                    (lambda () (display "test"))))
               (storage (init <git-subdir> d))
-              (path `(a b.txt))]
+              (path '(a b.txt)))
          (add storage test-file path #f)
-         (test-equal
-             (string-append d "/a/b.txt")
-             (get storage path '())))))))
+         (is (equal? (string-append d "/a/b.txt")
+                     (get storage path '()))))))))
