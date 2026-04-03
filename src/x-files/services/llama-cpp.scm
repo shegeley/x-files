88b4d7fccf94ae34da966382e765277e97ca2193
Grigory Shepelev <mail@grigory.tech>
2026-04-03 11:02:37 +0300

    (x-files services llama-cpp): moving

--- src/x-files/services/llama-cpp.scm
+++ src/x-files/services/llama-cpp.scm
@@ -1,22 +1,18 @@
 (define-module (x-files services llama-cpp)
 
-  #:use-module (gnu)
-  #:use-module (gnu packages)
-  #:use-module (gnu services)
   #:use-module ((gnu packages admin) #:select (shadow))
   #:use-module ((gnu packages machine-learning) #:select (llama-cpp))
-  #:use-module ((gnu system accounts) #:select (user-account
-                                                user-group
-                                                subid-range))
+  #:use-module ((gnu system accounts) #:select (user-account user-group))
   #:use-module ((gnu system shadow) #:select (account-service-type))
-
+  #:use-module ((gnu services) #:select (service-type service-extension))
+  #:use-module ((gnu services shepherd) #:select (shepherd-root-service-type
+                                                   shepherd-service))
   #:use-module (guix gexp)
-  #:use-module (guix packages)
-  #:use-module (guix git-download)
-  #:use-module (guix build-system copy)
-  #:use-module (gnu services shepherd))
+  #:use-module ((guix records) #:select (define-record-type*))
 
-(define str string-append)
+  #:export (llama-cpp-model
+            llama-cpp-configuration
+            llama-cpp-service-type))
 
 ;; models as packages is kinda pointless because default guix timeout on quite execution of git-fetch/lfs is 3600 seconds. the weight much more
 #;(define %gpt-oss-20b-gguf
@@ -37,22 +33,22 @@
        (description "")
        (license #f))))
 
-(define* (llama-cpp-default-config
-          #:key (model (str "/data/models"
-                            "/LFM2-8B-A1B-GGUF"
-                            "/LFM2-8B-A1B-Q8_0.gguf"))
-          (port "9090"))
-  (let* [(cmd #~(list
-                 #$(file-append llama-cpp "/bin/llama-server")
-                 "-m" #$model
-                 "-c" "16384"
-                 "-np" "4"
-                 "--port" port))]
-    `((command . ,#~(make-forkexec-constructor
-                     #$cmd
-                     #:log-file "/var/log/llama-cpp.log"
-                     #:user "llama-cpp"
-                     #:group "llama-cpp")))))
+(define-record-type* <llama-cpp-model>
+  llama-cpp-model make-llama-cpp-model
+  llama-cpp-model?
+  (name         llama-cpp-model-name)           ;; string, used in shepherd provision and log file name
+  (model-path   llama-cpp-model-path)           ;; path to .gguf file
+  (port         llama-cpp-model-port            ;; port string
+                (default "9090"))
+  (context-size llama-cpp-model-context-size    ;; context window size string
+                (default "16384"))
+  (parallel     llama-cpp-model-parallel        ;; number of parallel slots string
+                (default "4")))
+
+(define-record-type* <llama-cpp-configuration>
+  llama-cpp-configuration make-llama-cpp-configuration
+  llama-cpp-configuration?
+  (models llama-cpp-configuration-models))      ;; list of <llama-cpp-model>
 
 (define %llama-cpp-accounts
   (list
@@ -60,30 +56,53 @@
      (name "llama-cpp")
      (group "llama-cpp")
      (system? #t)
-     (comment "Lama-cpp daemon user")
+     (comment "Llama-cpp daemon user")
      (home-directory "/var/lib/llama-cpp")
      (shell (file-append shadow "/sbin/nologin")))
    (user-group
      (name "llama-cpp")
      (system? #t))))
 
-(define (llama-cpp-shepherd-service config)
-  (list
-   (shepherd-service
-     (provision     '(llama-cpp))
-     (requirement   '(user-processes networking))
-     (start         (assoc-ref config 'command))
-     (auto-start?   #t)
-     (stop          #~(make-kill-destructor))
-     (documentation "Run llama-cpp server"))))
+(define (llama-cpp-model->shepherd-service model)
+  (let* [(name         (llama-cpp-model-name model))
+         (model-path   (llama-cpp-model-path model))
+         (port         (llama-cpp-model-port model))
+         (context-size (llama-cpp-model-context-size model))
+         (parallel     (llama-cpp-model-parallel model))
+         (log-file     (string-append "/var/log/llama-cpp-" name ".log"))
+         (provision    (list (string->symbol (string-append "llama-cpp-" name))))]
+    (shepherd-service
+      (provision     provision)
+      (requirement   '(user-processes networking))
+      (start         #~(make-forkexec-constructor
+                        (list #$(file-append llama-cpp "/bin/llama-server")
+                              "-m" #$model-path
+                              "-c" #$context-size
+                              "-np" #$parallel
+                              "--port" #$port)
+                        #:log-file #$log-file
+                        #:user "llama-cpp"
+                        #:group "llama-cpp"))
+      (auto-start?   #t)
+      (stop          #~(make-kill-destructor))
+      (documentation (string-append "Run llama-cpp server for model: " name)))))
 
-(define-public llama-cpp-service-type
+(define (llama-cpp-shepherd-services config)
+  (map llama-cpp-model->shepherd-service
+       (llama-cpp-configuration-models config)))
+
+(define llama-cpp-service-type
   (service-type
     (name 'llama-cpp)
     (extensions
      (list (service-extension account-service-type
                               (const %llama-cpp-accounts))
            (service-extension shepherd-root-service-type
-                              llama-cpp-shepherd-service)))
-    (default-value (llama-cpp-default-config))
-    (description "Llama cpp")))
+                              llama-cpp-shepherd-services)))
+    (default-value
+      (llama-cpp-configuration
+       (models (list (llama-cpp-model
+                      (name "lfm2-8b")
+                      (model-path "/data/models/LFM2-8B-A1B-GGUF/LFM2-8B-A1B-Q8_0.gguf")
+                      (port "9090"))))))
+    (description "Llama.cpp inference server. Each model gets its own shepherd service and log file.")))
