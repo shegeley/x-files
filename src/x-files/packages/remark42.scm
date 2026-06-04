fa9e1c835220b8a9f59a6cf96f29e8d31a1b16fe
Grigory Shepelev <mail@grigory.tech>
2026-06-04 16:19:55 +0300

    (x-files packages remark42): 1.15.0 -> 1.16.1; add aarch64-linux

--- src/x-files/packages/remark42.scm
+++ src/x-files/packages/remark42.scm
@@ -5,27 +5,39 @@
   #:use-module (guix build-system copy)
   #:export (remark42))
 
+(define target->remark42-arch
+  '(("x86_64-linux"  . "amd64")
+    ("aarch64-linux" . "arm64")))
+
+(define target->remark42-hash
+  '(("x86_64-linux"  . "1b9sg3np5yl18v4r91xyfrhgdy4vjcxfggpisdly26idjbwyqjsa")
+    ("aarch64-linux" . "133865q6n7ic64jzsh3h9zak053n9pgjpxi2jkljzy23lvj58maq")))
+
 (define-public remark42
-  (package
-    (name "remark42")
-    (version "1.15.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (string-append
-             "https://github.com/umputun/remark42/releases/download/v"
-             version "/remark42.linux-amd64.tar.gz"))
-       (sha256
-        (base32 "1qwn7xhc7xw8bgkw3wsa8yd21ca5gfyy37fgkawh885c4gia01fz"))))
-    (build-system copy-build-system)
-    (arguments
-     `(#:install-plan
-       '(("remark42.linux-amd64" "bin/remark42"))))
-    (supported-systems '("x86_64-linux"))
+  (let* [(target (or (%current-target-system) (%current-system)))
+         (arch   (assoc-ref target->remark42-arch target))
+         (hash   (assoc-ref target->remark42-hash target))]
+    (package
+      (name "remark42")
+      (version "1.16.1")
+      (source
+       (origin
+         (method url-fetch)
+         (uri (string-append
+               "https://github.com/umputun/remark42/releases/download/v"
+               version "/remark42.linux-" arch ".tar.gz"))
+         (sha256
+          (base32 hash))))
+      (build-system copy-build-system)
+      (arguments
+       (list
+        #:install-plan
+        `'((,(string-append "remark42.linux-" arch) "bin/remark42"))))
+      (supported-systems (map car target->remark42-arch))
     (home-page "https://remark42.com")
     (synopsis "Privacy focused lightweight commenting engine")
     (description
      "Remark42 is a self-hosted, lightweight, and simple commenting system.
 It stores data in a single bolt database, requires no external dependencies,
 and supports multiple auth providers.")
-    (license license:expat)))
+    (license license:expat))))
