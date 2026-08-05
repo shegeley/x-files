04ee71702cc8ecee26cb852484e6689284017ac5
Grigory Shepelev <mail@grigory.tech>
2026-08-05 14:28:50 +0300

    (x-files packages kuber): k0s 1.36.2 ⇆ 1.36.3+k0s.0

--- src/x-files/packages/kuber.scm
+++ src/x-files/packages/kuber.scm
@@ -15,14 +15,14 @@
   (let ((version-suffix "+k0s.0"))
     (package
       (name "k0s")
-      (version "1.36.2")
+      (version "1.36.3")
       (source (origin
                 (method url-fetch)
                 (uri (string-append
                       "https://github.com/k0sproject/k0s/releases/download/v"
                       version version-suffix
                       "/k0s-v" version version-suffix "-amd64"))
-                (sha256 (base32 "164jis43p19ablrs71rgpl0fcj2v99bv407r8k5pmwixh1grhpcb"))))
+                (sha256 (base32 "1wp6vmpgw9j3mhh66qq99dlnd5iy541ig814pcipw9dhfl2n05fb"))))
       (native-inputs (list binutils coreutils))
       (build-system trivial-build-system)
       (arguments
